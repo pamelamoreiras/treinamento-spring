b021e9f75748ab16982191804b9871305bf29979
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-05-04 19:21:35 -0300

    commit do dia 04/05/2022

--- src/main/java/com/pamela/spring/Application.java
+++ src/main/java/com/pamela/spring/Application.java
@@ -1,10 +1,60 @@
 package com.pamela.spring;
 
+import com.pamela.spring.domain.entity.Cliente;
+import com.pamela.spring.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes){
+		return args -> {
+			System.out.println("Salvando Clientes");
+
+			clientes.salvar(new Cliente("Pamela"));
+			clientes.salvar(new Cliente("Alef"));
+			clientes.salvar(new Cliente("fulano"));
+
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+
+			System.out.println("Atualizando Clientes");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado");
+				clientes.atualizar(c);
+			});
+
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("Buscando Clientes");
+			clientes.buscarPorNome("ful").forEach(System.out::println);
+
+//			System.out.println("Deletando Clientes");
+//			clientes.obterTodos().forEach(c -> {
+//					clientes.deletar(c);
+//			});
+
+			todosClientes = clientes.obterTodos();
+			if (todosClientes.isEmpty()){
+				System.out.println("Nenhum Cliente Encontrado");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
+
+
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/pamela/spring/domain/repositorio/Clientes.java
@@ -0,0 +1,73 @@
+package com.pamela.spring.domain.repositorio;
+
+import com.pamela.spring.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class Clientes {
+
+    private static String INSERT = "insert into cliente (nome) values (?) ";
+
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+
+    private static String UPDATE = "update cliente set nome = ? where id = ?";
+
+    private static String DELETE = "delete from cliente where id = ? ";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Cliente salvar(Cliente cliente){
+        jdbcTemplate.update(INSERT, new Object[] {cliente.getNome()});
+        return cliente;
+    }
+
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()
+        });
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> obterTodos(){
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+
+    }
+
+
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                String nome = resultSet.getString("nome");
+                Integer id = resultSet.getInt("id");
+                return new Cliente(id, nome);
+            }
+        };
+    }
+
+}
