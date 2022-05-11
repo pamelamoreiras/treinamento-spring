316e40aadd1ffc73a70fd0384d488d74e48ce527
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-05-11 16:33:31 -0300

    refatorando para spring web com s verbos http

--- src/main/java/com/pamela/spring/Application.java
+++ src/main/java/com/pamela/spring/Application.java
@@ -1,45 +1,21 @@
 package com.pamela.spring;
 
 import com.pamela.spring.domain.entity.Cliente;
-import com.pamela.spring.domain.entity.Pedido;
 import com.pamela.spring.domain.repository.Clientes;
-import com.pamela.spring.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 @SpringBootApplication
 public class Application {
 
 	@Bean
-	public CommandLineRunner init(
-			@Autowired Clientes clientes,
-			@Autowired Pedidos pedidos
-	){
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
 		return args -> {
-			System.out.println("Salvando Clientes");
-			Cliente fulano = new Cliente("Fulano");
-			clientes.save(fulano);
-
-			Pedido p = new Pedido();
-			p.setCliente(fulano);
-			p.setDataPedido(LocalDate.now());
-			p.setTotal(BigDecimal.valueOf(100));
-
-			pedidos.save(p);
-
-//			Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-//			System.out.println(cliente);
-//			System.out.println(cliente.getPedidos());
-
-			pedidos.findByCliente(fulano).forEach(System.out::println);
-
-
+		Cliente c = new Cliente(null, "fulano");
+		clientes.save(c);
 		};
 	}
 
